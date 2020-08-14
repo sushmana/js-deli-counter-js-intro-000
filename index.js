b02834fa201cebe1cdd43ec0c52f159def10e06a
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 19:59:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-var katzDeliLine = [];
-
-function takeANumber(currentLine,newPerson){
-
-for i=currentLine[newPerson];
-  return `Welcome,{newPerson}. You are number ${i}in line`
-
+function takeANumber(katzDeliLine , name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(){
-
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nodoby waiting to be served!";
+  }
+  else {
+    return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
 }
+
+
+function currentLine(array){
+  var line = [];
+  if (array.length === 0){
+    return "The line is currently empty.";
+  }
+  else {
+    for (let i = 0 ; i < array.length ; i++){
+      line.push(` ${i+1}. ${array[i]}`)
+    }
+    return 'The line is currently:' + line
+  }
+}
+ ProTip! U
