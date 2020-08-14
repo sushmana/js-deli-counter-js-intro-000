56a27caa588b52872934bc7d17edb64b8ee464c8
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 19:57:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var katzDeliLine = [];
+
+function takeANumber(currentLine,newPerson){
+  
+for i=currentLine[newPerson];
+  return `Welcome,{newPerson}. You are number ${i}in line`
+
+}
+
+function nowServing(){
+  
+}
